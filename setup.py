6f45f3568d53667d1f2c645216c4f46dadcf2e07
nel215 <otomo.yuhei@gmail.com>
2016-07-25 20:40:14 +0900

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, find_packages
 
 setup(name='skopt',
       version='0.0',
-      description='Sequential model-based optimization toolbox.')
+      description='Sequential model-based optimization toolbox.',
+      packages=find_packages(),
+      py_modules=['skopt'])
